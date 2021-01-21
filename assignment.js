08b146796168b09a2111851a01cd84bc86834f28
Murad Ahmed <muradahmed199771@gmail.com>
2021-01-22 00:18:21 +0600

    second problem solved with all corner case

--- assignment.js
+++ assignment.js
@@ -6,28 +6,53 @@ megaFriend*/
 // kilometerToMeter function start from here
 function kilometerToMeter(kilometer) {
 
-    /*checking the number is equal or less then 0 and a number(empty string value is zero)
-     so we will also check the type of input here! 
-    if it's not number and value is 0, we will show the error messege more precisely in different error message)*/
-
+    
+        //unreal number exeption handling 
     if (kilometer <=0 && typeof kilometer == 'number' ) {
         const wrongValue = "Please give a real distance! no cheating!!";
         return wrongValue;
     }
-    //checking the type of input is number or not
-    else if (typeof kilometer !== 'number') {
+    //wrong data type expetion handling
+    else if (typeof kilometer !== 'number' && typeof kilometer !== 'undefined') {
         const wrongType = "Mistakenly you enterd a value which is not number. Please  a give number in input";
         return wrongType;
 
     }
-    else {
+    //main calculation
+    else if (kilometer > 0) {
         let meter = kilometer * 1000;
         return meter;
+    }
+    //empty call exeption handling
+    else{
+        var emptyValue = "You can not submit empty value! please eneter  distance in a number"
+        return emptyValue;
     }
 }
 
 //budgetCalculator function start here
-budgetCalculator{
 
+function budgetCalculator(watch, phone,laptop){
+    // negative value exeception handling
+    if (watch < 0 || phone < 0 || laptop < 0) {
+        const wrongValue = "Please enetr a real value! you can't buy  minus number of product";
+        return wrongValue;
+    }
+    //data type exception handling
+    else if(typeof watch !== 'number' || typeof phone !== 'number' || typeof laptop !== 'number'){
+        var notNumber = "Are you sure you entered a number? please enter your number correctly again"
+        return notNumber;
+    }
+    //main calculation
+    else {
+        watch *= 50;
+        phone *=  100;
+        laptop *= 500;
+        var total = watch + phone + laptop;
+        return total;
+
+    }
 }
+
+
 

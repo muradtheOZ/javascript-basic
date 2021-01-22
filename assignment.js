d28d747fba29c9ccaef342f0e8bccb5795dd73b7
Murad Ahmed <muradahmed199771@gmail.com>
2021-01-22 12:03:01 +0600

    third problem done with all corner case

--- assignment.js
+++ assignment.js
@@ -3,56 +3,112 @@ budgetCalculator ghori 50, phone 100, laptop 500;
 hotelCost
 megaFriend*/
 
-// kilometerToMeter function start from here
+
 function kilometerToMeter(kilometer) {
 
-    
-        //unreal number exeption handling 
-    if (kilometer <=0 && typeof kilometer == 'number' ) {
-        const wrongValue = "Please give a real distance! no cheating!!";
+    //unreal number exeption handling 
+    if (kilometer <= 0 && typeof kilometer == 'number') {
+        var wrongValue = "Please give a real distance! no cheating!!";
         return wrongValue;
     }
     //wrong data type expetion handling
     else if (typeof kilometer !== 'number' && typeof kilometer !== 'undefined') {
-        const wrongType = "Mistakenly you enterd a value which is not number. Please  a give number in input";
+        var wrongType = "Mistakenly you enterd a value which is not number. Please  a give number in input";
         return wrongType;
 
     }
     //main calculation
     else if (kilometer > 0) {
-        let meter = kilometer * 1000;
+        var meter = kilometer * 1000;
         return meter;
     }
     //empty call exeption handling
-    else{
+    else {
         var emptyValue = "You can not submit empty value! please eneter  distance in a number"
         return emptyValue;
     }
 }
 
-//budgetCalculator function start here
 
-function budgetCalculator(watch, phone,laptop){
+
+function budgetCalculator(watch, phone, laptop) {
     // negative value exeception handling
     if (watch < 0 || phone < 0 || laptop < 0) {
-        const wrongValue = "Please enetr a real value! you can't buy  minus number of product";
+        var wrongValue = "Please enetr a real value! you can't buy  minus number of product";
         return wrongValue;
     }
-    //data type exception handling
-    else if(typeof watch !== 'number' || typeof phone !== 'number' || typeof laptop !== 'number'){
+    //data type and empty call exception handling
+    else if (typeof watch !== 'number' || typeof phone !== 'number' || typeof laptop !== 'number') {
         var notNumber = "Are you sure you entered a number? please enter your number correctly again"
         return notNumber;
     }
     //main calculation
-    else {
+    else if (watch % 1 == 0 && phone % 1 == 0 && laptop % 1 == 0) {
         watch *= 50;
-        phone *=  100;
+        phone *= 100;
         laptop *= 500;
         var total = watch + phone + laptop;
         return total;
 
     }
+    //float number exception handling
+    else {
+        var floatCatch = "product number can not be float! please enter a real number!"
+        return floatCatch;
+    }
 }
+
+
+
+function hotelCost(days) {
+
+    //unreal number exeption handling
+    if (days <= 0 && typeof days == 'number') {
+        var wrongValue = "Days cant be 0 or less then 0!!";
+        return wrongValue;
+    }
+
+    //wrong data type expetion handling
+    else if (typeof days !== 'number' && typeof days !== 'undefined') {
+        var wrongType = "Mistakenly you enterd a value which is not number. Please  a give number in input";
+        return wrongType;
+
+    }
+    //float exception handling
+    else if (days % 1 !== 0 && typeof days == 'number') {
+        var floatCatch = "days can't be float! please enter a real number!"
+        return floatCatch;
+    }
+
+    //main calculation
+    else if (days > 0 && days <= 10) {
+        var totalRent = days * 100;
+        return totalRent;
+    }
+    else if (days > 10 && days <= 20) {
+        var firstTenDays = 10 * 100;
+        var remaining = (days - 10) * 80;
+        var totalRent = firstTenDays + remaining;
+        return totalRent;
+    }
+
+    else if (days > 20) {
+        var firstTenDays = 10 * 100;
+        var secondTenDays = 10 * 80;
+        var remaining = (days - 20) * 50;
+        var totalRent = firstTenDays + secondTenDays + remaining;
+        return totalRent;
+    }
+
+
+    //empty call exeption handling
+    else {
+        var emptyValue = "You can not submit empty value! please eneter a value"
+        return emptyValue;
+    }
+
+}
+
 
 
 
